c617358a59fbfb9d18c217c2a8f24a7e2dfecdb8
exploitpnk <exploitpnk@gmail.com>
2020-08-03 21:19:40 -0500

    [Add] create file with Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744',
+}
