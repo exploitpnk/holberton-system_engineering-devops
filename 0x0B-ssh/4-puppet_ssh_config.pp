0135f05446dd957bdfd45fccd9ea2d97d5e3481c
exploitpnk <exploitpnk@gmail.com>
2020-08-04 06:33:21 -0500

    [Add] Puppet SSH Manifiest

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Puppet Manifiest to customize SSH configuration
 file_line { 'PasswordAuthentication':
-  path => '/etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   ensure => 'present',
-  line => 'PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
 }
 file_line { 'IdentityFile':
   path   => '/etc/ssh/ssh_config',
