8d685d4441fce8225e67677f951057dd37ea43e1
exploitpnk <exploitpnk@gmail.com>
2020-08-26 16:51:37 -0500

    [Add] Puppet add headers

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,20 @@
 # Creating a custom HTTP header response with Puppet
 exec { 'update_system':
-	command => 'sudo apt-get update -y',
-	path    => [ '/usr/bin', '/bin' ],
+  command => 'sudo sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
 }
+
 exec { 'install_nginx':
-	command => 'sudo apt-get install nginx -y',
-	path    => [ '/usr/bin', '/bin' ],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
 }
-exec {'add_headers':
-	command => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default'
-	path	=> ['/usr/bin', '/bin' ],
+
+exec { 'apply_config':
+  command => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
+  path    => ['/usr/bin', '/bin'],
 }
-exec { 'restart_nginx':
+
+exec { 'restart_service':
   command => 'sudo service nginx start',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
 }
