b380ecee8930bd085688b4fe7847a81a3d9cf647
exploitpnk <exploitpnk@gmail.com>
2020-08-26 17:12:18 -0500

    [Add] Puppet add headers

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,19 +3,18 @@ exec { 'update':
   command => 'sudo apt-get update -y',
   path    => ['/usr/bin', '/bin'],
 }
-exec { 'install_nginx':
-  require => exec['update']
-  command => 'sudo apt-get install nginx -y',
-  path    => ['/usr/bin', '/bin'],
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
 }
 file_line { 'apply_config':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
   line    => 'add_header X-Served-By "$HOSTNAME";',
-  require => Package['install_nginx'],
+  require => Package['nginx'],
 }
 service { 'nginx':
   ensure  => running,
-  require => Package['install_nginx'],
+  require => Package['nginx'],
 }
